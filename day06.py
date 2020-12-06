945fc3ff1c68275367008794cbb0ef446072efd6
Lythi45 <scala.bine@gmail.com>
2020-12-06 09:22:54 +0100

    day6

---
+++ day06.py
@@ -0,0 +1,21 @@
+for fu in [lambda s1,s2:s1.union(s2),lambda s1,s2:s1.intersection(s2)]:
+    ng=True
+    ans=set()
+    su=0
+    for answers in map(lambda s:s.strip(),open("input06.txt",'r').readlines()):
+        if answers=='':
+            su+=len(ans)
+            ans=set()
+            ng=True
+        else:
+            if ng:
+                ans=set(list(answers))
+                ng=False
+            else:
+                ans=fu(ans,set(list(answers)))
+    print(su+len(ans))
+
+
+
+
+
