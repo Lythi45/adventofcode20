abf4445c5ce8f36a2864b53366d409e01a8150a3
Lythi45 <scala.bine@gmail.com>
2020-12-07 01:17:16 +0100

    day6 one liner added

--- day06.py
+++ day06.py
@@ -5,6 +5,7 @@ for fu in [lambda s1,s2:s1.union(s2),lambda s1,s2:s1.intersection(s2)]:
     for answers in map(lambda s:s.strip(),open("input06.txt",'r').readlines()):
         if answers=='':
             su+=len(ans)
+
             ans=set()
             ng=True
         else:
@@ -15,7 +16,8 @@ for fu in [lambda s1,s2:s1.union(s2),lambda s1,s2:s1.intersection(s2)]:
                 ans=fu(ans,set(list(answers)))
     print(su+len(ans))
 
+#one liner
+from functools import reduce 
 
-
-
+print([sum(map(len,[reduce(lambda x,y:fu(x,y),block) for block in map(lambda s:map(set,s.split('\n')),open("input06.txt",'r').read().split('\n\n'))])) for fu in [lambda s1,s2:s1.union(s2),lambda s1,s2:s1.intersection(s2)]])
 
