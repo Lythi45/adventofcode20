727b02c8d49f54adf0c9a9216dcf9f4cd3b9a4d7
Lythi45 <scala.bine@gmail.com>
2020-12-09 21:09:44 +0100

    day 9 a bit enhanced

--- day09.py
+++ day09.py
@@ -7,13 +7,15 @@ for i in range(25,len(nums)):
     if num not in prenums:
         print(num)
         break
+# Sucht nach einen Bereich von nums, der zusammenaddiert so groß wie num ist
+# und gibt die Summe der ersten und letzten Zahl dieses Bereiches aus
 ranges=combinations(range(len(nums)),2)
-rangesums=map(lambda x:(sum(nums[x[0]:x[1]+1]),x[0],x[1]),ranges)
-sums={}
-for x in rangesums:
-        sums[x[0]]=(x[1],x[2])
-minmax=sums[num]
-print(nums[minmax[0]]+nums[minmax[1]])
+rangesums=map(lambda x:(sum(nums[x[0]:x[1]+1]),nums[0]+nums[x[1]]),ranges)
+minmaxsums={}
+#for x in rangesums:
+#    if x[0]==num:
+#       print(x[1])
 
+print([x[1] for x in rangesums if x[0]==num])
 
 
