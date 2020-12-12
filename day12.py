2f6949873f90e6b2717d1f7abaa7ca2619e17047
Lythi45 <scala.bine@gmail.com>
2020-12-12 18:35:30 +0100

    day12"

--- day12.py
+++ day12.py
@@ -1,48 +1,42 @@
 hr={'N':0,'E':1,'S':2,'W':3}
 ri=[(0,1),(1,0),(0,-1),(-1,0)]
-sx=0
-sy=0
-sr=1
-for line in open("input12.txt",'r').readlines():
-    r=line[0]
-    dist=int(line[1:])
-    if r=='F':
-        sx+=ri[sr][0]*dist
-        sy+=ri[sr][1]*dist
-    elif r=='R':
-        sr=(sr+(dist//90))%4
-    elif r=='L':
-        sr=(sr-(dist//90))%4
-    else:
-        sx+=ri[hr[r]][0]*dist
-        sy+=ri[hr[r]][1]*dist
-print(abs(sx)+abs(sy))
-
-hr={'N':0,'E':1,'S':2,'W':3}
-ri=[(0,1),(1,0),(0,-1),(-1,0)]
-sx=0
-sy=0
 wx=10
 wy=1
 sr=1
-for line in open("input12.txt",'r').readlines():
-    r=line[0]
-    dist=int(line[1:])
-    
-    osr=sr
-    if r=='F':
-        sx+=dist*wx
-        sy+=dist*wy
-    elif r=='R':
-        sr=(sr+(dist//90))%4
-    elif r=='L':
-        sr=(sr-(dist//90))%4
-    else:
-        wx+=ri[hr[r]][0]*dist
-        wy+=ri[hr[r]][1]*dist
-    
-    while osr!=sr:
-        wx,wy=(wy,-wx)
-        osr=(osr+1)%4
 
-print(abs(sx)+abs(sy))
+for ship in range(2):
+    sx=0
+    sy=0
+    for line in open("input12.txt",'r').readlines():
+        r=line[0]
+        dist=int(line[1:])
+        
+        if ship==0:
+            if r=='F':
+                sx+=ri[sr][0]*dist
+                sy+=ri[sr][1]*dist
+            elif r=='R':
+                sr=(sr+(dist//90))%4
+            elif r=='L':
+                sr=(sr-(dist//90))%4
+            else:
+                sx+=ri[hr[r]][0]*dist
+                sy+=ri[hr[r]][1]*dist
+        else:
+            wr=0
+            if r=='F':
+                sx+=dist*wx
+                sy+=dist*wy
+            elif r=='R':
+                wr=(dist//90)%4
+            elif r=='L':
+                wr=(-dist//90)%4
+            else:
+                wx+=ri[hr[r]][0]*dist
+                wy+=ri[hr[r]][1]*dist
+            
+            while wr!=0:
+                wx,wy=(wy,-wx)
+                wr=(wr-1)%4
+
+    print(abs(sx)+abs(sy))
