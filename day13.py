22ecea4ef15a69b39f0e27ad9b37585fae891974
Lythi45 <scala.bine@gmail.com>
2020-12-13 18:06:22 +0100

    day 13

--- day13.py
+++ day13.py
@@ -1,13 +1,12 @@
 lines=open("input13.txt",'r').readlines()
 time=int(lines[0])
-b_takt=[int(x) for x in lines[1].split(',') if x!='x']
-bus_arrivals=sorted(list(map(lambda x: (x-time%x,x),b_takt)),key=lambda x:x[0])
+buses=[(x[0],int(x[1])) for x in enumerate(lines[1].split(',')) if x[1]!='x']
+bus_arrivals=sorted(list(map(lambda x: (x[1]-time%x[1],x[1]),buses)),key=lambda x:x[0])
 print(bus_arrivals[0][0]*bus_arrivals[0][1])
 
 a=0
 b=1
-buses=[(x[0],int(x[1])) for x in enumerate(lines[1].split(',')) if x[1]!='x']
-for diff,takt in  buses:
+for diff,takt in buses:
     while (a+diff)%takt!=0:
         a+=b
     b*=takt
