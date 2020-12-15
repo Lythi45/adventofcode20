2143a71c17ea51a953d064d6e2db47f1042294c6
Lythi45 <scala.bine@gmail.com>
2020-12-15 11:43:22 +0100

    day 15

---
+++ day15.py
@@ -0,0 +1,13 @@
+num_list=[0,13,1,8,6,15]
+for max_turn in [2020,30000000]:
+    last_num=num_list[-1]
+    turns=len(num_list)
+    nums=dict(zip(num_list[:-1],range(turns-1)))
+    while turns<max_turn:
+        new_num=turns-nums[last_num]-1 if last_num in nums else 0
+        nums[last_num]=turns-1
+        last_num=new_num
+        turns+=1
+    print(new_num)
+
+
